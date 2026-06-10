c0af414bb2e4e3dd44350ae43acc6d832388d341
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2026-06-10 15:52:40 +0530

    complete working state of uart hal

--- common.hpp
+++ common.hpp
@@ -1,10 +1,14 @@
 #ifndef __HAL_COMMON__
 #define __HAL_COMMON__
+extern uint32_t SystemCoreClock;
 
 struct Callback{
     void (*fn)(void*) = nullptr;
     void* ctx = nullptr;
     void invoke() const {if(fn) fn(ctx);}
 };
+
+template<typename T>
+T&& move(T& x) { return static_cast<T&&>(x); }
 
 #endif // __HAL_COMMON__
